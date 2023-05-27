c2367a87fd0d3c6034423c25db397fbd0ed73ab8
Pedro Leta <pedrogleta@gmail.com>
2023-05-26 21:23:10 -0300

    add extra sidebar components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,21 @@
+import { DashboardIcon } from '@/components/icons/DashboardIcon';
+import { ItemsIcon } from '@/components/icons/ItemsIcon';
 import { TransactionsIcon } from '@/components/icons/TransactionsIcon';
 
 export default function Home() {
   return (
     <div>
-      <div className="h-8 flex ">Header</div>
-      <div className="flex flex-row">
+      <div className="h-16 flex ">Header</div>
+      <div className="flex flex-row ml-3">
         <div className="flex flex-col">
-          <div className="flex flex-row">
-            <TransactionsIcon /> Transactions
+          <div className="flex flex-row border border-indigo-500 rounded px-6 py-2">
+            <TransactionsIcon /> <p className="ml-2">Transactions</p>
+          </div>
+          <div className="flex flex-row border border-indigo-500 rounded px-6 py-2 mt-3">
+            <DashboardIcon /> <p className="ml-2">Dashboard</p>
+          </div>
+          <div className="flex flex-row border border-indigo-500 rounded px-6 py-2 mt-3">
+            <ItemsIcon /> <p className="ml-2">Itens</p>
           </div>
         </div>
         <div className="ml-4">Main Section</div>
