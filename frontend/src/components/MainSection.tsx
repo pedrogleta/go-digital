9bbb08aa03afbcb2c9b7d359f272d9ff90cc3f70
Pedro Leta <pedrogleta@gmail.com>
2023-05-26 21:34:17 -0300

    add main section headers

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -1,5 +1,9 @@
 export const MainSection = () => (
-  <div className="ml-4">
-    <h1>Main Section</h1>
+  <div className="ml-4 w-1/2">
+    <div className="flex flex-row w-full">
+      <h2 className="mr-auto">Nome</h2>
+      <h2 className="mr-8">Quantidade</h2>
+      <h2>Preço</h2>
+    </div>
   </div>
 );
