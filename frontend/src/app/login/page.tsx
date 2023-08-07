8990cdc8c56439120e77944c40ec8d73b36292ba
Rodrigo Moreira Ribeiro <mulukou@gmail.com>
2023-08-07 15:56:34 -0300

    initial work for auth

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  getRedirectResult,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../lib/firebase-config";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  function signIn(email: string, password: string) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((v) => {
+        router.push("/home");
+      })
+      .catch((err) => {
+        console.error("An error occurred on signIn: ", err);
+      });
+  }
+
+  function signUp(email: string, password: string) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((v) => {
+        router.push("/home");
+      })
+      .catch((err) => {
+        console.error("An error occurred on signIn: ", err);
+      });
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        className="border-red-300 border-4"
+        placeholder="email"
+        onChange={(v) => {
+          setEmail(v.target.value);
+        }}
+      />
+      <input
+        type="text"
+        className="border-red-300 border-4"
+        placeholder="password"
+        onChange={(v) => {
+          setPassword(v.target.value);
+        }}
+      />
+      <button onClick={() => signIn(email, password)}>Sign In</button>
+      <button className="ml-5" onClick={() => signUp(email, password)}>
+        Sign Up
+      </button>
+    </>
+  );
+}
